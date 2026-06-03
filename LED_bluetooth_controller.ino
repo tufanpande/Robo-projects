39c86bcd79639c70653a1dfbdf021b3fb7ac2aa7
Tufan Pandey <99518915+tufanpande@users.noreply.github.com>
2026-06-03 11:27:45 +0545

    Update LED_bluetooth_controller.ino

--- LED_bluetooth_controller.ino
+++ LED_bluetooth_controller.ino
@@ -1,19 +1,46 @@
-char Incoming_value = 0;
-void setup (){
-  Serial.begin (9600);
-  pinMode (13, OUTPUT) ;
+#include <SoftwareSerial.h>
 
+SoftwareSerial BT(10, 11); // RX, TX
+
+#define RED_LED 7
+#define GREEN_LED 8
+
+char data;
+
+void setup() {
+
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
+  Serial.begin(9600);
+  BT.begin(9600);
+
+  Serial.println("Bluetooth LED Controller Ready");
 }
 
-void loop (){
-  if (Serial.available () > 0)
-  {
-    Incoming_value = Serial.read ();
-    Serial.print (Incoming_value);
-    Serial print ("\n");
-    if (Incoming_value == '1') 
-      digitalwrite (13, HIGH) ;
-    else if (Incoming_value == '0')
-    digitalwrite (13, LOW);
+void loop() {
+
+  if (BT.available()) {
+
+    data = BT.read();
+
+    Serial.print("Received: ");
+    Serial.println(data);
+
+    if (data == '1') {
+      digitalWrite(RED_LED, HIGH);
+    }
+
+    else if (data == '0') {
+      digitalWrite(RED_LED, LOW);
+    }
+
+    else if (data == '2') {
+      digitalWrite(GREEN_LED, HIGH);
+    }
+
+    else if (data == '3') {
+      digitalWrite(GREEN_LED, LOW);
+    }
   }
 }
